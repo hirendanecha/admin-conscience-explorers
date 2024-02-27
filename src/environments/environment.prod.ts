bae64d904e713d77e80014132a4ca62ffee6a654
Swapnil Opash <sra.opash@gmail.com>
2024-02-27 12:05:12 +0530

    fixed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   hmr: false,
-  serverUrl: 'https://api.freedom.buzz/api/v1/',
+  serverUrl: 'https://api.conscienceexplorers.com/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://api.freedom.buzz/',
+  socketUrl: 'https://api.conscienceexplorers.com/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
 
